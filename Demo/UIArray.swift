91dec74a492890bb63183c0c20df9f810ad90d0a
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-12-02 11:20:09 +0900

    Add Array

---
+++ Demo/UIArray.swift
@@ -0,0 +1,23 @@
+//
+//  UIArray.swift
+//  Demo
+//
+//  Created by 築山朋紀 on 2018/12/02.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import Foundation
+
+public extension Array where Element: Equatable {
+    @discardableResult
+    public mutating func remove(element: Element) -> Index? {
+        guard let index = index(of: element) else { return nil }
+        remove(at: index)
+        return index
+    }
+    
+    @discardableResult
+    public mutating func remove(elements: [Element]) -> [Index] {
+        return elements.compactMap { remove(element: $0) }
+    }
+}
